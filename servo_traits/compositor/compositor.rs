293eaa1e334cf54515467a9ad0412009ed3a8fa3
Paul Rouget <me@paulrouget.com>
2016-11-11 03:53:35 +0100

    add notes to viewport

--- servo_traits/compositor/compositor.rs
+++ servo_traits/compositor/compositor.rs
@@ -24,19 +24,23 @@ pub struct Animation {
 }
 
 pub struct ViewFrame {
-    coordinates: Rect<i32>,
+    viewport: glViewport, // http://docs.gl/gl3/glViewport
     z_index: i32,
     background_color: Color,
     opacity: f32,
 }
 
 pub struct ContentFrame {
+    // Content frame is screen_rect here:
+    // https://github.com/servo/webrender/blob/00a8818d97aa651ac4670ad85c429729c2046e9d/webrender/src/tiling.rs#L2041
     coordinates: Rect<f32>,
     pixel_ratio: f32,
     scroll_position: Point2D<f32>,
 }
 
 pub trait View {
+    // glViewport is set here:
+    // https://github.com/servo/webrender/blob/00a8818d97aa651ac4670ad85c429729c2046e9d/webrender/src/device.rs#L894
     fn get_frame(&self) -> ViewFrame;
     fn set_frame(&self, frame: ViewFrame, animation: Option<Animation>);
     fn is_under_point(&self, point: Point2D<u32>) -> bool;
@@ -50,7 +54,7 @@ pub trait Compositor {
     fn new_browserview(&self, outer_frame: ViewFrame, content_frame: ContentFrame) -> BrowserView;
     fn get_browserviews(&self) -> Iterator<BrowserView>;
     fn get_browserviews_from_point(&self, Point2D<f32>) -> Iterator<Item = BrowserView>;
-    fn new_documentview(&self, frame:  ViewFrame, document: DocumentID) -> View;
+    fn new_documentview(&self, frame:  ViewFrame, document: DocumentID) -> DocumentView;
 }
 
 pub trait DocumentView : View {
